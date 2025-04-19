c31eb3b49de02f11fbea29131e2c53e9890f43a3
Edgar Muyomba <edgarmatthew247@gmail.com>
2025-04-19 19:22:10 +0300

    hustling sdk versions

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -3,10 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:summerbody/blocs/Schedule/schedule_bloc.dart';
 import 'package:summerbody/routing/routes.dart';
-import 'package:logger/logger.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
@@ -15,27 +14,10 @@ class Splash extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: BlocListener<ScheduleBloc, ScheduleState>(
-      // bloc: context.read<ScheduleBloc>(),
       listener: (context, state) async {
-        DateTime now = DateTime.now();
-        String currentDay = now.weekday == DateTime.monday
-            ? 'monday'
-            : now.weekday == DateTime.tuesday
-                ? 'tuesday'
-                : now.weekday == DateTime.wednesday
-                    ? 'wednesday'
-                    : now.weekday == DateTime.thursday
-                        ? 'thursday'
-                        : now.weekday == DateTime.friday
-                            ? 'friday'
-                            : now.weekday == DateTime.saturday
-                                ? 'saturday'
-                                : 'sunday';
-
         if (state is ScheduleReady) {
           Future.delayed(
-              const Duration(seconds: 2),
-              () => context.goNamed(Routes.day));
+              const Duration(seconds: 2), () => context.goNamed(Routes.day));
         }
       },
       child: Padding(
