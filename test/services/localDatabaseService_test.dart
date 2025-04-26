61652a67709faefeda948f2a6abd556b27749a52
Edgar Muyomba <edgarmatthew247@gmail.com>
2025-04-26 23:02:07 +0300

    wip: testing

---
+++ test/services/localDatabaseService_test.dart
@@ -0,0 +1,77 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:summerbody/database/daos/EntryDao.dart';
+import 'package:summerbody/database/daos/MuscleGroupDao.dart';
+import 'package:summerbody/database/daos/WorkoutDao.dart';
+import 'package:summerbody/database/database.dart';
+import 'package:summerbody/database/tables/MuscleGroup.dart';
+import 'package:summerbody/services/LocalDatabaseService.dart';
+
+@GenerateMocks([AppDatabase, MuscleGroupDao, WorkoutDao, EntryDao])
+import 'localDatabaseService_test.mocks.dart';
+
+final List<MuscleGroup> mockMuscleGroups = [
+  MuscleGroup(1, 'Chest', '', 'assets/icons/chest.png'),
+  MuscleGroup(2, 'Arms', '', 'assets/icons/arms.png'),
+  MuscleGroup(3, 'Shoulders', '', 'assets/icons/shoulders.png'),
+  MuscleGroup(4, 'Back', '', 'assets/icons/back.png'),
+  MuscleGroup(5, 'Legs', '', 'assets/icons/legs.png'),
+];
+
+void main() {
+  late MockAppDatabase mockAppDatabase;
+  late MockMuscleGroupDao mockMuscleGroupDao;
+  late MockWorkoutDao mockWorkoutDao;
+  late MockEntryDao mockEntryDao;
+  late LocalDatabaseService localDatabaseService;
+
+  setUp(() {
+    mockAppDatabase = MockAppDatabase();
+    mockMuscleGroupDao = MockMuscleGroupDao();
+    mockWorkoutDao = MockWorkoutDao();
+    mockEntryDao = MockEntryDao();
+
+    when(mockAppDatabase.muscleGroupDao).thenReturn(mockMuscleGroupDao);
+    when(mockAppDatabase.workoutDao).thenReturn(mockWorkoutDao);
+    when(mockAppDatabase.entryDao).thenReturn(mockEntryDao);
+
+    localDatabaseService = LocalDatabaseService(appDatabase: mockAppDatabase);
+  });
+
+  group("Local Database Service Tests", () {
+    test("get all muscle groups", () async {
+      when(mockMuscleGroupDao.getAllMuscleGroups())
+          .thenAnswer((_) async => mockMuscleGroups);
+
+      final result = await localDatabaseService.getAllMuscleGroups();
+
+      expect(result, mockMuscleGroups);
+      expect(result.length, 5);
+      verify(mockAppDatabase.muscleGroupDao).called(1);
+    });
+
+    test("Get muscle group by key", () async {
+      when(mockMuscleGroupDao.getMuscleGroupById(1)).thenAnswer(
+          (realInvocation) async =>
+              MuscleGroup(1, 'Chest', '', 'assets/icons/chest.png'));
+
+      when(mockMuscleGroupDao.getMuscleGroupsByName("Legs")).thenAnswer(
+          (realInvocation) async =>
+              [MuscleGroup(5, 'Legs', '', 'assets/icons/legs.png')]);
+
+      when(mockMuscleGroupDao.getMuscleGroupsByDay("Monday"))
+          .thenAnswer((realInvocation) async => []);
+
+      final result1 = await localDatabaseService.getMuscleGroupByKey("id", 1);
+      final result2 =
+          await localDatabaseService.getMuscleGroupByKey("name", "Legs");
+      final result3 =
+          await localDatabaseService.getMuscleGroupByKey("day", "Monday");
+
+      expect(result1, MuscleGroup(1, 'Chest', '', 'assets/icons/chest.png'));
+      expect(result2, MuscleGroup(5, 'Legs', '', 'assets/icons/legs.png'));
+      expect(result3, isNull);
+    });
+  });
+}
